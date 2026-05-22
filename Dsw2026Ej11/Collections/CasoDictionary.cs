47b99b2efaaa6fe97f6e72af94df835f5c335b04
lujan cano <lujancano40@gmail.com>
2026-05-21 23:24:16 -0300

    ejercicios completados

--- Dsw2026Ej11/Collections/CasoDictionary.cs
+++ Dsw2026Ej11/Collections/CasoDictionary.cs
@@ -7,4 +7,51 @@
 //Incluir un método para eliminar un alumno utilizando la clave
 public class CasoDictionary
 {
+   
+    private Dictionary<int, Alumno> alumnos;
+
+    public CasoDictionary()
+    {
+        alumnos = new Dictionary<int, Alumno>();
+    }
+    public void AgregarAlumno(int legajo, Alumno alumno)
+    {
+        alumnos.Add(legajo, alumno);
+    }
+    public Alumno BuscarAlumno(int legajo)
+    {
+        if (alumnos.ContainsKey(legajo))
+        {
+            return alumnos[legajo];
+        }
+
+        return null;
+    }
+    public Dictionary<int, Alumno> ObtenerAlumnos()
+    {
+        return alumnos;
+    }
+    public void EliminarAlumno(int legajo)
+    {
+        alumnos.Remove(legajo);
+    }
+}
+
+    public class Alumno
+{
+    public int Id { get; }
+    public string Nombre { get; }
+    public double Promedio { get; }
+
+    public Alumno(int id, string nombre, double promedio)
+    {
+        Id = id;
+        Nombre = nombre;
+        Promedio = promedio;
+    }
+
+    public override string ToString()
+    {
+        return $"{Id} - {Nombre} - Promedio: {Promedio}";
+    }
 }
